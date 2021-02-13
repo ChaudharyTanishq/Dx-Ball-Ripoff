cd058f38636f7c76be78f8ff1875bd3070ae4c71
chaudharytanishq <tanishqchaudhary511@gmail.com>
2021-02-13 10:32:05 +0530

    Upd: Decreasing the game size

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import random
 from colorama import Fore, Back, Style
 
 # SCREEN PARAMETERS
-SCREEN_WIDTH = 42
-SCREEN_HEIGHT = 24
+SCREEN_WIDTH = 32
+SCREEN_HEIGHT = 16
 SCREEN = []
 for i in range(SCREEN_HEIGHT):
     SCREEN.append([' ' for _ in range(SCREEN_WIDTH)])
@@ -14,8 +14,12 @@ FRAME_FCOLOR = Fore.BLACK
 
 # BAR PARAMETERS
 bar_start_x = random.randint(10, SCREEN_WIDTH-10)
-INIT_BAR_START = (bar_start_x, -3)
-INIT_BAR_END = (bar_start_x+4, -3)
+INIT_BAR_START = (bar_start_x, SCREEN_HEIGHT-3)
+INIT_BAR_END = (bar_start_x+4, SCREEN_HEIGHT-3)
 BAR_SPEED = 1
-BAR_BCOLOR = Back.WHITE
+BAR_BCOLOR = Back.GREEN
 BAR_FCOLOR = Fore.RESET
+
+# BALL PARAMETERS
+BALL_BCOLOR = Back.RESET
+BALL_FCOLOR = Fore.MAGENTA
