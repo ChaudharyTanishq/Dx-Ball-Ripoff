934f0cca2be5968c42a4a6ea833054287ebad945
chaudharytanishq <tanishqchaudhary511@gmail.com>
2021-02-13 10:33:10 +0530

    Upd: Reflecting & integrating changes

--- Helpers/main.py
+++ Helpers/main.py
@@ -1,20 +1,31 @@
+from Components.Frame import Frame
+from Components.Bar import Bar
+from Components.Ball import Ball
 import os
 
 class Game():
-    def __init__(self, screen, bar):
+    def __init__(self, screen):
         """
         input params:
-            frame:
-                the frame object
-            bar:
-                the bar object
+            screen:
+                the screen to be used for drawing
         """
-        self.screen = screen
-        self.bar = bar
-        self.c = None
-        # renders the screen only when needed
-        self.re_render = True
+        self.frame = Frame() # frame object
+        self.frame.draw(screen) # leaving the frame mark on the screen 
+        self.screen = screen # screen
+        
+        self.bar = Bar() # bar object
+        self.ball = Ball() # ball object
+        self.c = None # the character that was inputted
         return
+    
+    def handle_ball(self):
+        """
+        handles ball logic
+        """
+        self.ball.draw(self.screen, reset=True)
+        self.ball.move()
+        self.ball.draw(self.screen)
 
     def handle_input(self, c):
         """
@@ -26,27 +37,37 @@ class Game():
             self.c = c
             self.bar.draw(self.screen, reset=True)
             self.bar.move(True if c == 'a' else False)
-            # need to re render after bar move.
-            self.re_render = True
+    
+    def handle_collisions(self):
+        """
+        handles all the collision cases.
+        """
+        self.frame.handle_collision(self.ball)
+        self.bar.handle_collision(self.ball)
 
     def render(self):
         """
         renders the components on the given screen
-        """
-        # quit if not needed.
-        if not self.re_render:
-            # print('yeet')
-            return
-        
+        """        
+
         # adding the components
-        os.system('clear')
+        # draws the bar/paddle
         self.bar.draw(self.screen)
+        # handles all the drawing logic for the ball
+        self.handle_ball()
+
+        # handling all the collision logic
+        self.handle_collisions()
 
         # printing the components on the screen
+        os.system('clear')
         for row in self.screen:
             for char in row:
                 print(char, end="")
             print()
         
-        # done rendering. stopping till required again.
-        self.re_render = False
+        # checking if the game is over or not
+        if self.ball.over:
+            return True
+        else:
+            return False
