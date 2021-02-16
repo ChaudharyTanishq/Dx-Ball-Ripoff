47cb91cd909e2747c7c539f3300b87501f38b03f
chaudharytanishq <tanishqchaudhary511@gmail.com>
2021-02-16 13:00:10 +0530

    Add: Ball velocity limiter, Upd: Ball init

--- Components/Ball.py
+++ Components/Ball.py
@@ -1,20 +1,25 @@
-from config import BALL_BCOLOR, BALL_FCOLOR, SCREEN_HEIGHT, SCREEN_WIDTH
+from config import BALL_BCOLOR, BALL_FCOLOR, BALL_X, BALL_Y, SCREEN_HEIGHT
 from colorama import Style
+import random
 
 class Ball():
     def __init__(self):
-        self.position = (SCREEN_WIDTH//2, SCREEN_HEIGHT-15)
+        self.position = (
+            BALL_X,
+            BALL_Y
+        )
         # down is positive
         self.velocity = (0, 1)
         self.bcolor = BALL_BCOLOR
         self.fcolor = BALL_FCOLOR
         self.over = False
+        self.through = False
     
     def reset(self):
         """
         resets the mouse pointer to 1, 1
         """
-        print(f'\033[{SCREEN_HEIGHT};{0}H', end='')
+        print(f'\033[{SCREEN_HEIGHT+1};{1}H', end='')
         print()
     
     def color_string(self, reset):
@@ -30,7 +35,18 @@ class Ball():
     def move(self):
         x, y = self.position
         x_v, y_v = self.velocity
-        self.position = (x+x_v, y+y_v)
 
+        # limiting velocities
+        if x_v > 2:
+            x_v = 2
+        if x_v < -2:
+            x_v = -2
+        if y_v > 2:
+            y_v = 2
+        if y_v < -2:
+            y_v = -2
+        
+        self.velocity = (x_v, y_v)
+        self.position = (x+x_v, y+y_v)
         if self.position[1] >= SCREEN_HEIGHT-2:
             self.over = True
