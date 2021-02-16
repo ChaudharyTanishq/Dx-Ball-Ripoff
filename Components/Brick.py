ffb4f0b6f54527469036ddfee0ebfd8f79149bc9
chaudharytanishq <tanishqchaudhary511@gmail.com>
2021-02-16 13:01:28 +0530

    Add: Drawing for bricks

--- Components/Brick.py
+++ Components/Brick.py
@@ -14,7 +14,9 @@ class Brick(Surface):
     def __init__(self, start, end, strength):
         bcolor = None
         fcolor = None
-        
+        self.strength = strength
+        # self.yeet = False
+
         if strength == 1:
             bcolor = BRICK_1_BCOLOR
             fcolor = BRICK_1_FCOLOR
@@ -27,7 +29,7 @@ class Brick(Surface):
         elif strength == 4:
             bcolor = BRICK_4_BCOLOR
             fcolor = BRICK_4_FCOLOR
-        
+   
         super().__init__(
             start,
             end,
@@ -36,6 +38,41 @@ class Brick(Surface):
             fcolor
         )
 
-# class Bricks():
-#     def __init__(self):
+    def draw(self, reset):
+        """
+        draws the box on screen
+
+        input prams:
+            reset:
+                instead of colouring, it decolours
+        """
+        x1, y1 = self.start
+        x2, y2 = self.end
+        assert y1 == y2
+        print(f'\033[{y1+1};{x1+1}H', end='')
+        for _ in range(x2-x1+1):
+            print(self.color_string(reset), end='')
+        self.reset()
+
+    def handle_collision(self, ball):
+        # gets the direction after collision checking
+        direction = super().handle_collision(ball)
+
+        if not direction:
+            return None
+
+        if ball.through:
+            # inverting collisions, since ball has to 
+            # 'cut-through'
+            x_v, y_v = ball.velocity 
+            
+            if direction == 'x':
+                x_v *= -1
+            elif direction == 'y':
+                y_v *= -1
+
+            # saving ball's new velocity
+            ball.velocity = x_v, y_v
+            return 'through'
         
+        return direction
