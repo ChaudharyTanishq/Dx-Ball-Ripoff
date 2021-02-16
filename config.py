e8edf177c8c6b93c4b9fb7d22dc84e525310315e
chaudharytanishq <tanishqchaudhary511@gmail.com>
2021-02-16 13:46:39 +0530

    Add: Basic bar and frame colouring

---
+++ config.py
@@ -0,0 +1,11 @@
+import random
+from colorama import Fore, Back, Style
+
+SCREEN_WIDTH = 16
+SCREEN_HEIGHT = 32
+
+FRAME_BCOLOR = Back.BLACK
+FRAME_FCOLOR = Fore.RESET
+
+BAR_BCOLOR = Back.GREEN
+BAR_FCOLOR = Fore.RESET
