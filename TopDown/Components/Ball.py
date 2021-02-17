64fab90046d2cf7bb18a3ad8c197abbfd297af57
chaudharytanishq <tanishqchaudhary511@gmail.com>
2021-02-17 13:47:15 +0530

    Upd: specialised ball movements and added fast powerup

--- TopDown/Components/Ball.py
+++ TopDown/Components/Ball.py
@@ -1,18 +1,27 @@
-from config import BAR_END, SCREEN_HEIGHT
+from config import BAR_END, BAR_SPEED, BAR_START, SCREEN_HEIGHT
 from Components.Point import Point
 
 class Ball(Point):
-    def __init__(self, position):
+    def __init__(self, position, velocity=(0, 1)):
         super().__init__(
             symbol='O', 
             position=position, 
-            through=False
+            through=False,
+            velocity=velocity
         )
-        self.over = False
+        self.sticky = False
+        self.saved_velocity = None
 
-    def move(self):
-        super().move()
+    def fast(self):
+        xv, yv = self.velocity
+        if yv > 4:
+            return
+        self.velocity = xv, yv*2
 
-        _, y = self.position
-        if y >= SCREEN_HEIGHT-2:
-            self.over = True
+    def move(self, bar=None, direction=None):
+        if not bar:
+            super().move()
+        else:
+            direction = -1 if direction else 1
+            x, y = self.position
+            self.position = x + direction*BAR_SPEED, y
