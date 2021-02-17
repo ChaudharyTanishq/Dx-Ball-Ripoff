43095eca2a1750d97a54f612fc66cff1281e36ef
chaudharytanishq <tanishqchaudhary511@gmail.com>
2021-02-17 13:47:35 +0530

    Add: handling bar for sticky powerup

--- TopDown/Components/Bar.py
+++ TopDown/Components/Bar.py
@@ -1,3 +1,4 @@
+from Components.Powerup import Powerup
 from config import BAR_BCOLOR, BAR_END, BAR_FCOLOR, BAR_SPEED, BAR_START, SCREEN_WIDTH
 from Components.Surface import Surface
 
@@ -11,7 +12,7 @@ class Bar(Surface):
             bcolor=BAR_BCOLOR, 
             fcolor=BAR_FCOLOR
         )
-    
+
     def grow(self):
         x1, y1 = self.start
         x2, y2 = self.end
@@ -42,30 +43,38 @@ class Bar(Surface):
         left = -1 if left else 1
         
         # updating x positions
-        x1 = x1 + left*BAR_SPEED
-        x2 = x2 + left*BAR_SPEED
+        t1 = x1 + left*BAR_SPEED
+        t2 = x2 + left*BAR_SPEED
 
         # checking validity of x positions
-        while x1 < 0:
-            x1 += 1
-            x2 += 1
-        while x2 > SCREEN_WIDTH-1:
-            x1 -= 1
-            x2 -= 1
+        if t1 <0 or t2 > SCREEN_WIDTH:
+            return False
+        # while x1 < 0:
+        #     x1 += 1
+        #     x2 += 1
+        # while x2 > SCREEN_WIDTH-1:
+        #     x1 -= 1
+        #     x2 -= 1
 
         # saving the values
-        self.start = (x1, y1)
-        self.end = (x2, y2)
+        self.start = (t1, y1)
+        self.end = (t2, y2)
+        return True
 
     # reflects the point (mostly written with ball in mind)
     def handle_collided(self, point):
         direction = self.is_collided(point)
 
+        # if no collision at all
         if not direction:
             return None
         
-        # reflect the point always
-        self.reflect(point, direction)
+        # if powerup
+        if isinstance(point, Powerup):
+            return True
+
+        # only ball object reaches here
+        unsaved_velocity = None
 
         # the logic for angled point returns
         if direction == 'y':
@@ -77,4 +86,26 @@ class Bar(Surface):
             xv = x3-centre
             if xv > 2: xv = 2
             if xv < -2: xv = -2
-            point.velocity = (xv, yv)
+            if not point.sticky:
+                point.velocity = (xv, yv)
+            else:
+                unsaved_velocity = (xv, yv)
+        
+        # reflect the point if not sticky
+        if not point.sticky: 
+            self.reflect(point, direction)
+        else:
+            try:
+                xv, yv = unsaved_velocity 
+                if direction == 'x':
+                    xv *= -1
+                elif direction == 'y':
+                    yv *= -1
+                unsaved_velocity = (xv, yv)
+            except:
+                return None
+        # if already saved from before, dont save again
+        if point.saved_velocity:
+            return None
+        else:
+            return unsaved_velocity
